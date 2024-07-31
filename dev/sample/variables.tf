3440400f6d182d407ce8af8be7adfcc9c35182fa
xwen11 <102186192+xwen11@users.noreply.github.com>
2024-07-31 09:50:56 -0700

    Create files

--- dev/sample/variables.tf
+++ dev/sample/variables.tf
@@ -8,32 +8,59 @@ variable "localAdminUser" {
   sensitive   = true
   type        = string
 }
+
 variable "localAdminPassword" {
   description = "The password of the local administrator account."
   sensitive   = true
   type        = string
 }
+
 variable "domainAdminUser" {
   description = "The username of the domain account."
   sensitive   = true
   type        = string
 }
+
 variable "domainAdminPassword" {
   description = "The password of the domain account."
   sensitive   = true
   type        = string
 }
+
+variable "deploymentUserPassword" {
+  sensitive   = true
+  type        = string
+  description = "The password for deployment user."
+}
+
 variable "servicePrincipalId" {
   description = "The id of service principal to create hci cluster."
   sensitive   = true
   type        = string
 }
+
 variable "servicePrincipalSecret" {
   description = "The secret of service principal to create hci cluster."
   sensitive   = true
   type        = string
 }
-variable "tenant" {
-  description = "The tenant of hci cluster."
+
+variable "rpServicePrincipalObjectId" {
+  default     = ""
   type        = string
+  description = "The object ID of the HCI resource provider service principal."
+}
+
+variable "vmAdminPassword" {
+  description = "Admin password for the VM"
+  type        = string
+  sensitive   = true
+  default     = ""
+}
+
+variable "domainJoinPassword" {
+  description = "Password of User with permissions to join the domain."
+  type        = string
+  sensitive   = true
+  default     = ""
 }
